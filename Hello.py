66591422aeac22489f546723302b92a4975610db
Sihan Huang <sh3453@columbia.edu>
2017-10-18 18:00:51 -0400

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,9 @@
+import os
+import time
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
